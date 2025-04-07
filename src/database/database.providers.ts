fac547cae3e0ec243d3d5a07e226552a89901d31
Vlad <vladkygot@gmail.com>
2025-04-07 21:33:02 +0300

    create Railway constant

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -12,12 +12,12 @@ import { News } from '@/news/entities/news.entity';
 
 require('dotenv').config();
 
-const { 
-    DB_HOST,
-    DB_USERNAME,
-    DB_PASSWORD,
-    DB_DATABASE,
-    DB_PORT 
+const {
+  MYSQLHOST,
+  MYSQLUSER,
+  MYSQLPASSWORD,
+  MYSQLDATABASE,
+  MYSQLPORT
 } = process.env;
 
 const MYSQL_DIALECT = "mysql";
@@ -25,29 +25,29 @@ const MYSQL_DIALECT = "mysql";
 // ============== end CONSTANTS only section
 
 export const databaseProviders = [
-    {
-      provide: SEQUELIZE,
-      useFactory: async () => {
-        const sequelize = new Sequelize({
-          dialect: MYSQL_DIALECT,
-          host: DB_HOST,
-          port: DB_PORT ? +DB_PORT : 3306, // Should be number
-          username: DB_USERNAME,
-          password: DB_PASSWORD,
-          database: DB_DATABASE,
+  {
+    provide: SEQUELIZE,
+    useFactory: async () => {
+      const sequelize = new Sequelize({
+        dialect: MYSQL_DIALECT,
+        host: MYSQLHOST,
+        port: MYSQLPORT ? +MYSQLPORT : 3306, // Should be number
+        username: MYSQLUSER,
+        password: MYSQLPASSWORD,
+        database: MYSQLDATABASE,
       });
-  
-        /**
-         * Add Models Here
-         * ===============
-         */
-        sequelize.addModels([
-          User, Hospital, Vaccine, Patient, Record, VaccineList, News,
-        ]);
-  
-        //await sequelize.sync({ force: true }); //TODO: only for testing - When your application starts up, this deletes all tables including the data in them, and recreate them from scratch
-        await sequelize.sync(); //TODO: This only creates new tables that don’t exist and if an existing table has changed it won’t update it. This is not recommended for production but it is suitable for development
-        return sequelize;
-      },
+
+      /**
+       * Add Models Here
+       * ===============
+       */
+      sequelize.addModels([
+        User, Hospital, Vaccine, Patient, Record, VaccineList, News,
+      ]);
+
+      //await sequelize.sync({ force: true }); //TODO: only for testing - When your application starts up, this deletes all tables including the data in them, and recreate them from scratch
+      await sequelize.sync(); //TODO: This only creates new tables that don’t exist and if an existing table has changed it won’t update it. This is not recommended for production but it is suitable for development
+      return sequelize;
     },
-  ];
+  },
+];
